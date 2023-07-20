5a2b7cf6ec92edd6a9b4095e5da9d237fbecf00d
Julien Malka <julien@malka.sh>
2023-07-20 12:56:32 +0200

    Problem: The prebuilt binary available is not compatible with other architectures like aarch64-linux. It is also not compatible with exotic, non-FHS compliant OSes like NixOS.
    Solution: Build the software from source using Nix.

---
+++ passveil.nix
@@ -0,0 +1,83 @@
+{ mkDerivation
+, aeson
+, ansi-terminal
+, base
+, base16-bytestring
+, bytestring
+, containers
+, cryptohash-sha256
+, directory
+, filepath
+, hashable
+, hexstring
+, lib
+, megaparsec
+, mtl
+, optparse-applicative
+, process
+, random
+, regex-base
+, regex-pcre
+, temporary
+, text
+, time
+, unix
+, unordered-containers
+, fetchzip
+}:
+
+mkDerivation {
+  pname = "passveil";
+  version = "0.1.0.0";
+  src = fetchzip {
+    url = "https://hub.darcs.net/antei/passveil/dist";
+    sha256 = "sha256-0Hee/snKz09T5QSrL+n8gkzYFN+0JWHE6vfR9N2wTeQ=";
+    extension = "zip";
+  };
+
+  patches = [ ./passveil-cabal.patch ];
+
+  isLibrary = true;
+  isExecutable = true;
+  jailbreak = true;
+  libraryHaskellDepends = [
+    aeson
+    base
+    base16-bytestring
+    bytestring
+    cryptohash-sha256
+    directory
+    filepath
+    hashable
+    hexstring
+    megaparsec
+    mtl
+    process
+    random
+    text
+    time
+    unordered-containers
+  ];
+  executableHaskellDepends = [
+    hexstring
+    aeson
+    ansi-terminal
+    base
+    bytestring
+    containers
+    directory
+    filepath
+    mtl
+    optparse-applicative
+    process
+    regex-base
+    regex-pcre
+    temporary
+    text
+    time
+    unix
+    unordered-containers
+  ];
+  license = "unknown";
+  #  mainProgram = "passveil";
+}
