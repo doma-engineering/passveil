bd2e5d114722dcd0d47a0d8e1f44b79e4417b6e0
Ilona <ixahedron@users.noreply.github.com>
2024-09-11 18:14:46 +0200

    Add a nix flake
    Problem: no nix flakes and I can't run the dynamically built binary
    on NixOS.
    
    Solution: packaged passveil through haskell-flake. Also, it's been
    forked 3 years ago and deps are outdated, so bumped them. Also also,
    brought some maintenance changes over from the original, like
    raichoo's email.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Doma fork of passveil";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [
+        inputs.haskell-flake.flakeModule
+      ];
+      perSystem = { self', system, lib, config, pkgs, ... }: {
+        haskellProjects.default = {
+
+          # For `nix develop`:
+          devShell.tools = hp: {
+            inherit (pkgs)
+              pkg-config
+              darcs
+
+              # docs
+              pandoc;
+          };
+
+        };
+        # For `nix build` & `nix run`:
+        packages.default = self'.packages.passveil;
+      };
+    };
+}
