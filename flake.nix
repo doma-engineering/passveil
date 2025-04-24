d8cd5c777777fe62903af082cbb07f3473ae0dbd
cognivore <66186054+cognivore@users.noreply.github.com>
2025-04-24 01:26:58 +0100

    Problem: No mac os support
    Solution:
    
     - Support mac os!

--- flake.nix
+++ flake.nix
@@ -1,52 +1,42 @@
 {
   description = "Doma fork of passveil";
+
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url  = "github:hercules-ci/flake-parts";
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixpkgs-unstable";
     haskell-flake.url = "github:srid/haskell-flake";
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
       imports = [
         inputs.haskell-flake.flakeModule
       ];
+
       perSystem = { pkgs, ... }:
       let
         hp = pkgs.haskellPackages;
 
-        # Define the base passveil package
         passveil = hp.callCabal2nix "passveil" ./. {};
 
-        # Override attributes to add postInstall and nativeBuildInputs
         passveilWithCompletions = passveil.overrideAttrs (oldAttrs: {
           postInstall = ''
             ${oldAttrs.postInstall or ""}
             install -Dm644 contrib/bash/completions/passveil.bash "$out/share/bash-completion/completions/passveil"
             install -Dm644 contrib/zsh/completions/_passveil "$out/share/zsh/site-functions/_passveil"
             install -Dm644 contrib/fish/completions/passveil.fish "$out/share/fish/vendor_completions.d/passveil.fish"
-
-	    install -Dm755 scripts/passveil_unsafe_rollback "$out/bin/passveil_unsafe_rollback"
+            install -Dm755 scripts/passveil_unsafe_rollback "$out/bin/passveil_unsafe_rollback"
           '';
         });
       in {
         haskellProjects.default = {
-          # For `nix develop`:
           devShell.tools = hp: {
-            inherit (pkgs)
-              pkg-config
-              darcs
-
-              # lint
-              hlint
-
-              # docs
-              pandoc;
+            inherit (pkgs) pkg-config darcs hlint pandoc;
           };
         };
 
-        # For `nix build` & `nix run`:
         packages.default = passveilWithCompletions;
       };
     };
